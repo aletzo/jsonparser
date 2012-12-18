7b5fe13bb5d1cf3b8227be97a39c8046659982f7
aletzo <ajo@mydevnull.net>
2012-12-18 18:14:11 +0100

    No need for bootstrap js

--- index.php
+++ index.php
@@ -36,7 +36,6 @@ require_once 'lib/bootstrap.php';
     </div>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-1.8.3.min.js"><\/script>')</script>
-    <script src="bootstrap/js/bootstrap.min.js"></script>
     <script src="js/app.js"></script>
 </body>
 </html>

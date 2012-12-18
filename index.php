a014e1677f7f02d6c680d19f6544c76426e06688
aletzo <ajo@mydevnull.net>
2012-12-18 18:12:20 +0100

    Add html title

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ require_once 'lib/bootstrap.php';
 <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-    <title></title>
-    <meta name="description" content="">
+    <title>JSONparser</title>
     <meta name="viewport" content="width=device-width">
 
     <link rel="stylesheet" href="bootstrap/css/bootstrap.min.css">
